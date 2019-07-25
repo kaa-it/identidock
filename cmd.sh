5acdb743229ce4a7753c781a4b1b0e93d9eb3085
Andrey Kruglov <kruglov.it@gmail.com>
2019-07-25 20:14:03 +0300

    Initial commit

---
+++ cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+if [ "$ENV" = 'DEV' ]; then
+  echo "Running Development Server" # Запуск сервера для разработки
+  exec python "identidock.py"
+else
+  echo "Running Production Server" # Запуск сервера для эксплуатации
+  exec uswgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+             --callable app --stats 0.0.0.0:9191
+fi
