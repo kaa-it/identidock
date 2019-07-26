d02f0cf535a2b187647aeff3bd227648a875bf1b
Andrey Kruglov <kruglov.it@gmail.com>
2019-07-26 19:54:37 +0300

    jenkins

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,9 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
   echo "Running Development Server" # Запуск сервера для разработки
   exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+  echo "Running Unit Tests"
+  exec python "tests.py"
 else
   echo "Running Production Server" # Запуск сервера для эксплуатации
   exec uswgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
